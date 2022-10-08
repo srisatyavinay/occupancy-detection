bb15dbcd7a3e47272ac17fa3653a09827d40fc63
pegasus29 <akash2009janapati@gmail.com>
2022-10-08 14:03:41 +0530

    Uploaded Dashboard code

---
+++ Dashboard/src/layouts/dashboard/AccountPopover.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+// @mui
+import { Divider, Stack, MenuItem } from '@mui/material';
+// components
+import MenuPopover from '../../components/MenuPopover';
+
+// ----------------------------------------------------------------------
+
+const MENU_OPTIONS = [
+  {
+    label: 'Home',
+    icon: 'eva:home-fill',
+    linkTo: '/',
+  },
+  {
+    label: 'Profile',
+    icon: 'eva:person-fill',
+    linkTo: '#',
+  },
+  {
+    label: 'Settings',
+    icon: 'eva:settings-2-fill',
+    linkTo: '#',
+  },
+];
+
+// ----------------------------------------------------------------------
+
+export default function AccountPopover() {
+  const [open, setOpen] = useState(null);
+
+  const handleClose = () => {
+    setOpen(null);
+  };
+
+  return (
+    <>
+      <MenuPopover
+        open={Boolean(open)}
+        anchorEl={open}
+        onClose={handleClose}
+        sx={{
+          p: 0,
+          mt: 1.5,
+          ml: 0.75,
+          '& .MuiMenuItem-root': {
+            typography: 'body2',
+            borderRadius: 0.75,
+          },
+        }}
+      >
+        <Divider sx={{ borderStyle: 'dashed' }} />
+
+        <Stack sx={{ p: 1 }}>
+          {MENU_OPTIONS.map((option) => (
+            <MenuItem key={option.label} to={option.linkTo} component={RouterLink} onClick={handleClose}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </Stack>
+
+        <Divider sx={{ borderStyle: 'dashed' }} />
+
+        <MenuItem onClick={handleClose} sx={{ m: 1 }}>
+          Logout
+        </MenuItem>
+      </MenuPopover>
+    </>
+  );
+}
---
+++ Dashboard/src/routes.js
@@ -0,0 +1,27 @@
+import { Navigate, useRoutes } from 'react-router-dom';
+// layouts
+import DashboardLayout from './layouts/dashboard';
+import LogoOnlyLayout from './layouts/LogoOnlyLayout';
+import DashboardApp from './pages/DashboardApp';
+
+// ----------------------------------------------------------------------
+
+export default function Router() {
+  return useRoutes([
+    {
+      path: '/dashboard',
+      element: <DashboardLayout />,
+      children: [
+        { path: 'app', element: <DashboardApp /> },
+      ],
+    },
+    {
+      path: '/',
+      element: <LogoOnlyLayout />,
+      children: [
+        { path: '/', element: <Navigate to="/dashboard/app" /> },
+        { path: '*', element: <Navigate to="/404" /> },
+      ],
+    },
+  ]);
+}
